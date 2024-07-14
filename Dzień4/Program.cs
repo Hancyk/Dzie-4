18aabf24c837264a6494eae399d5f03abdd8671c
Hancyk Hanc <hancyk7@gmail.com>
2024-07-14 15:26:50 +0200

    Dodaj pliki projektów.

---
+++ Dzień4/Program.cs
@@ -0,0 +1,19 @@
+﻿var name = "Adam";
+bool sex = false;
+// mężczyzna - false, kobieta - true
+var age = 15;
+if (sex == true)
+{
+    if (age < 30)
+    {
+        Console.WriteLine("Kobieta poniżej lat 30");
+    }
+    if (name == "Ewa" && age == 30)
+    {
+        Console.WriteLine("Ewa lat 30");
+    }
+}
+else if (age < 18)
+{
+    Console.WriteLine("Niepełnoletni mężczyzna");
+}
